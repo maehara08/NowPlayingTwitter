44bdc90c91933503c4af8d75dc1b14afb5beeebc
maehara08 <aso.riiiku08@gmail.com>
2016-09-23 00:53:09 +0900

    socket io

--- public/javascripts/now-playing.js
+++ public/javascripts/now-playing.js
@@ -4,21 +4,52 @@
 
 window.onload = function (argument) {
 
+    //Queue
+    function Queue() {
+        this.__a = new Array();
+    }
+
+    Queue.prototype.size = function () {
+        return this.__a.length;
+    };
+
+    Queue.prototype.get = function (i) {
+        return this.__a[i];
+    };
+
+    Queue.prototype.dequeue = function () {
+        if (this.__a.length > 0) {
+            return this.__a.shift();
+        }
+        return null;
+    };
+
+    Queue.prototype.enqueue = function (o) {
+        if (this.size() > 200) {
+            this.dequeue();
+        }
+        this.__a.push(o);
+
+    };
+
+
     //var lyric = "i couldn't take it couldn't stand another minute couldn't bear another day without you in it";
     var lyric = "i couldn't take it couldn't stand another minute couldn't bear another day without you in it all of the joy that I had known for my life was stripped away from me the minute that you died to have you in my life was all i ever wanted but now without you I'm a soul forever haunted can't help but feel that i had taken you for granted no way in hell that i can ever comprehend this i wasn't dreaming when they told me you were gone i was wide awake and feeling that they had to be wrong how could you leave me when you swore that you would stay now i'm trapped inside a nightmare every single fucking day it's like a movie but there's not a happy ending every scene fades black and there's no pretending this little fairy tale doesn't seem to end well theres no knight in shining armor who will wake me from the spell i know you didn't plan this you tried to do what's right but in the middle of this madness i'm the one you left to win this fight red like roses fills my head with dreams and finds me always closer to the emptiness and sadness that has come to take the place of you i know you're broken down by anger and by sadness you feel I left you in a world that's full of madness wish i could talk to you if only for a minute make you understand the reasons why i did it i wanna tell you that you're all that ever mattered want you to know that for eternity i'm shattered i tried so hard just to protect you but i failed to and in a prison of abandonment i've jailed you i never planned that i would leave you there alone i was sure that i would see you when i made it back home and all the times I swore that it would be okay now i'm nothing but a liar and you're thrown into the fray this bedtime story ends with misery ever after the pages are torn and there's no final chapter i didn't have a choice I did what I had to do i made a sacrifice but forced a bigger sacrifice on you i know you've lived a nightmare i caused you so much pain but baby please don't do what i did i don't want you to waste your life in vain red like roses fills my head with dreams and finds me always closer to the emptiness and sadness that has come to take the place of you you're not the only one who needed me i thought you understood you were the one i needed and you left me as I always feared you would would I change it if i could? it doesn't matter how the petals scatter now every nightmare just discloses it's your blood that's red like roses and no matter what I do nothing ever takes the place of you red like roses fills my head with dreams and finds me always closer to the emptiness and sadness that has come to take the place of you";
+    var tweetQueue = new Queue();
+
     var words = {};
     var words_attr = [];
     string_handle(lyric);
 
     var canvas = document.getElementById('c');
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight/4*3;
+    canvas.height = window.innerHeight / 4 * 3;
 
     // canvas.width = window.innerWidth/2;
     // canvas.height = window.innerHeight/2;
 
     if (canvas.getContext) {
-         c = canvas.getContext('2d'),
+        c = canvas.getContext('2d'),
             w = canvas.width,
             h = canvas.height;
 
@@ -29,21 +60,21 @@ window.onload = function (argument) {
         // constructor
         Word = function (key) {
             this.text = key;
-            this.x = Math.random() * w;
+            this.font = Math.random() * 20  + 'px arial';
+            this.width = c.measureText(this.text).width ;
+            this.x = -this.width*2;
             this.y = Math.random() * h;
-            this.font = words[key] * 10 + 'px arial';
-            this.speed = (words[key]);
+            // this.font = words[key] * 10 + 'px arial';
+            // this.speed = 10;
+            // this.speed = c.measureText(this.text).width;
+            this.speed = 5;
+            // this.speed = (words[key]);
         };
-        for (key in words) {
-            words_attr.push(new Word(key));
-        }
-        console.log(words_attr.length);
 
         function animation() {
-            for (var i = 0; i < words_attr.length; i++) {
-                c.font = words_attr[i].font;
-                c.fillText(words_attr[i].text, words_attr[i].x, words_attr[i].y);
-                words_attr[i].width = c.measureText(words_attr[i].text).width;
+            for (var i = 0; i < tweetQueue.size(); i++) {
+                c.font = tweetQueue.get(i).font;
+                c.fillText(tweetQueue.get(i).text, tweetQueue.get(i).x, tweetQueue.get(i).y);
                 c.stroke();
             }
             move();
@@ -51,12 +82,17 @@ window.onload = function (argument) {
 
 
         function move() {
-            for (var i = 0; i < words_attr.length; i++) {
-                if (words_attr[i].x > w) {
-                    words_attr[i].x = -words_attr[i].width;
-                    words_attr[i].y = Math.random() * h;
+            for (var i = 0; i < tweetQueue.size(); i++) {
+                if (tweetQueue.get(i).x  > w) {
+                    tweetQueue.get(i).text="";
+                    // tweetQueue.get(i).font = Math.random() * 30 + 1 + 'px arial';
+                    // tweetQueue.get(i).width = c.measureText(tweetQueue.get(i).text).width;
+                    // tweetQueue.get(i).x = -tweetQueue.get(i).width;
+                    // tweetQueue.get(i).y = Math.random() * h;
+                    // tweetQueue.get(i).speed = Math.random() * 2+3;
+
                 } else {
-                    words_attr[i].x += words_attr[i].speed;
+                    tweetQueue.get(i).x += tweetQueue.get(i).speed;
                 }
             }
         }
@@ -67,30 +103,52 @@ window.onload = function (argument) {
         }, 24);
 
     }
+    // tweetQueue.enqueue(new Word('aaaaaaaaa'));
 
     function string_handle(str) {
-        var split_str = str.split(" ");
-        var word_array = [];
-        var word_count = [];
-        for (var i = 0; i < split_str.length; i++) {
-            check = true;
-            for (var j = 0; j <= word_array.length; j++) {
-                if (split_str[i] == word_array[j]) {
-                    word_count[j]++;
-                    check = false;
-                    break;
-                }
-            }
-            if (check) {
-                word_array.push(split_str[i]);
-                word_count.push(1);
-            }
-        }
-        for (var i = 0; i < word_array.length; i++) {
-            words[word_array[i]] = word_count[i];
-        }
-        return words;
+        var deleteUrl = str.replace(/http(s)?:([\/\w-.\/?%&=]*)?/g, "");
+        var deleteNewLine = deleteUrl.replace(/\n/g, " ");
+        return deleteNewLine;
+        // var split_str = str.split(" ");
+        // var word_array = [];
+        // var word_count = [];
+        // for (var i = 0; i < split_str.length; i++) {
+        //     check = true;
+        //     for (var j = 0; j <= word_array.length; j++) {
+        //         if (split_str[i] == word_array[j]) {
+        //             word_count[j]++;
+        //             check = false;
+        //             break;
+        //         }
+        //     }
+        //     if (check) {
+        //         word_array.push(split_str[i]);
+        //         word_count.push(1);
+        //     }
+        // }
+        // for (var i = 0; i < word_array.length; i++) {
+        //     words[word_array[i]] = word_count[i];
+        // }
+        // return words;
+
     }
+
+    $(function () {
+        var socket = io.connect();
+        $('form').submit(function () {
+            socket.emit('msg', $('input').val());
+            $('input').val('');
+            return false;
+        });
+
+        socket.on('msg', function (data) {
+            var resizeTweet = string_handle(data);
+            console.log(data);
+            console.log(resizeTweet);
+            tweetQueue.enqueue(new Word(resizeTweet));
+        });
+    });
+
 
     var queue = null, // キューをストック
         wait = 300; // 0.3秒後に実行の場合
@@ -103,8 +161,8 @@ window.onload = function (argument) {
         queue = setTimeout(function () {
 // リサイズ時に行う処理
             canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight/4*3;
-                w = canvas.width,
+            canvas.height = window.innerHeight / 4 * 3;
+            w = canvas.width,
                 h = canvas.height;
 
             c.strokeStyle = 'red';
--- routes/NowPlaying.js
+++ routes/NowPlaying.js
@@ -3,9 +3,9 @@
  */
 var express = require('express');
 var router = express.Router();
-var www=require('../bin/www');
-var io=www.io;
+var io=require('../bin/www');
 var Twit = require('twit');
+
 
 var T = new Twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -15,7 +15,6 @@ var T = new Twit({
     timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
 });
 
-console.log(io);
 
 // JavaScriptに関するつぶやきを表示する ---- (※2)
 var stream = T.stream('statuses/filter', {track: 'なうぷれ,nowplaying'});
@@ -23,29 +22,35 @@ var stream = T.stream('statuses/filter', {track: 'なうぷれ,nowplaying'});
 stream.on('tweet', function (tw) {
     var text = tw.text;
     var user_name = tw.user.name;
-    console.log(user_name + "> " + text);
+    io.io.sockets.emit('msg',text);
+    // console.log(user_name + "> " + text);
 });
 
-T.get('search/tweets', {q: '山手線', count: 30}, function (err, data) {
-    console.log(data);
-    var statuses = data['statuses'];
-    if (err) {
-        console.error("Twitter,Now Playing Error!")
-        console.error(err);
-    }
-    else {
-        for (var i = statuses.length - 1; i >= 0; i--) {
-            var user_name = statuses[i].user.name;
-            var text = statuses[i].text;
-            console.log(i + ' : ' + user_name + ' > ' + text);
-        }
-    }
-});
+// T.get('search/tweets', {q: '山手線', count: 30}, function (err, data) {
+//     console.log(data);
+//     var statuses = data['statuses'];
+//     if (err) {
+//         console.error("Twitter,Now Playing Error!")
+//         console.error(err);
+//     }
+//     else {
+//         for (var i = statuses.length - 1; i >= 0; i--) {
+//             var user_name = statuses[i].user.name;
+//             var text = statuses[i].text;
+//             console.log(i + ' : ' + user_name + ' > ' + text);
+//         }
+//     }
+// });
+
 
 /* GET now playing page. */
 router.get('/', function (req, res, next) {
     // res.render('now-playing');
     // res.render('index', { title: 'Express' });
+
+    // var www=require('../bin/www');
+    // var io=www.io;
+
     res.render('now-playing', {
         title: 'login demo',
         session: req.session.passport //passportでログイン後は、このオブジェクトに情報が格納されます。
